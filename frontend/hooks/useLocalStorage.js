5aa10c4bf34467fe56fdad38dd8c1eac56cff6a6
Roberto Andino <andinorobert518@yahoo.com>
2024-08-27 17:33:34 -0400

    Dark survives page reload

---
+++ frontend/hooks/useLocalStorage.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export function useLocalStorage(key, value) {
+    const [v, setV] = useState(() => {
+        const initial = localStorage.getItem(key)
+        return initial ? JSON.parse(initial) : value
+    })
+
+    const setValueLS = value => {
+        setV(value)
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+    return [v, setValueLS]
+}
